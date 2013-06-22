c3a2ffdd44a4cfe7d8341a4dc02b83760e2c9f81
Yosuke ONOUE <onoue@likr-lab.com>
2013-06-22 08:41:55 -0400

    Reverted ECBB implementation

--- src/strap/mmkp/algorithm/ecbb.cpp
+++ src/strap/mmkp/algorithm/ecbb.cpp
@@ -26,8 +26,10 @@ class Solver
 public:
   Solver(const Problem<PType, WType>& problem, const Index& index)
     : problem_(problem), index_(index), constraint_index_(problem.constraint_index()),
+      optimal_value_(0),
       solution_(problem.class_data(0)), current_solution_(problem.class_data(0)),
-      sub_lmck_problems_(problem.m())
+      upper_bound_(0), sub_lmck_problems_(problem.m()),
+      d_(problem.d()), m_(problem.m())
   {
     mck_problem_ = surrogate_constraints(problem_, index_);
     upper_bounds_ = mckp::algorithm::greedy_upper_bounds(*mck_problem_, index_);
@@ -35,18 +37,27 @@ public:
         new mckp::algorithm::LpRelaxationProblem<PType, double>(*mck_problem_, index_);
     base_solution_ = lmck_problem_->solve_with_solution();
 
-    PType upper_bound(0);
     for (const auto& klass : index_) {
       const int i = klass.i();
       for (const int j : klass) {
-        upper_bound = std::max(upper_bound, upper_bounds_->get(i, j));
+        upper_bound_ = std::max(upper_bound_, upper_bounds_->get(i, j));
       }
     }
 
-    make_entropy_core(*upper_bounds_, upper_bound, PType(0), index_);
+    obj_stack_.resize(m_);
+    c_stack_.resize(m_ * d_);
+    uc_stack_.resize(m_);
+
+    std::copy(
+        problem_.c_begin(),
+        problem_.c_end(),
+        c_stack_.begin());
+    uc_stack_[0] = mck_problem_->c();
+
+    make_entropy_core(*upper_bounds_, upper_bound_, PType(0), index_);
     sub_lmck_problems_[0] = new mckp::algorithm::LpRelaxationProblem<PType, double>(*lmck_problem_);
     sub_lmck_problems_[0]->remove(index_.at(0).i());
-    for (int depth = 1; depth < problem_.m(); ++depth) {
+    for (int depth = 1; depth < m_; ++depth) {
       sub_lmck_problems_[depth] = new mckp::algorithm::LpRelaxationProblem<PType, double>(*sub_lmck_problems_[depth - 1]);
       sub_lmck_problems_[depth]->remove(index_.at(depth).i());
     }
@@ -55,70 +66,8 @@ public:
 
   ClassIndexedData<int>* operator()()
   {
-    const int d = problem_.d();
-    const int m = problem_.m();
-    PType incumbent_value(0);
-    auto* solution = new ClassIndexedData<int>(m);
-    auto* incumbent_solution = new ClassIndexedData<int>(m);
-    std::vector<PType> obj_stack(m);
-    std::vector<WType> c_stack(m * d);
-    std::vector<double> uc_stack(m);
-    std::copy(
-        problem_.c_begin(),
-        problem_.c_end(),
-        c_stack.begin());
-    uc_stack[0] = mck_problem_->c();
-    auto bottom_class = index_.at(m - 1);
-    const int bottom_i = bottom_class.i();
-    bottom_class.sort([&](const int j1, const int j2) {
-          return problem_.p(bottom_class.i(), j1) >= problem_.p(bottom_class.i(), j2);
-        });
-
-
-    int leaf_count = 0;
-    index_.traverse([&, d, m](const int depth, const int i, const int j) -> bool {
-          solution->get(i) = j;
-          const auto& item = problem_.item(i, j);
-          if (upper_bounds_->get(i, j) < incumbent_value) {
-            return false;
-          }
-          if (!this->is_feasible(c_stack.begin() + d * depth, item.w_begin(), d)) {
-            return false;
-          }
-          const PType ub = sub_lmck_problems_[depth]->solve(uc_stack[depth] - mck_problem_->w(i, j));
-          if (obj_stack[depth] + item.p() + ub < incumbent_value) {
-            return false;
-          }
-
-          obj_stack[depth + 1] = obj_stack[depth] + item.p();
-          uc_stack[depth + 1] = uc_stack[depth] - mck_problem_->w(i, j);
-          for (const int k : constraint_index_) {
-            c_stack[d * (depth + 1) + k] = c_stack[d * depth + k] - item.w(k);
-          }
-
-          if (depth == m - 2) {
-            for (const int bottom_j : bottom_class) {
-              const auto& bottom_item = problem_.item(bottom_i, bottom_j);
-              if (obj_stack.back() + bottom_item.p() < incumbent_value) {
-                break;
-              }
-              ++leaf_count;
-              if (this->is_feasible(c_stack.begin() + d * (depth + 1), bottom_item.w_begin(), d)) {
-                solution->get(bottom_i) = bottom_j;
-                incumbent_value = obj_stack.back() + bottom_item.p();
-                for (const auto& klass : this->index_) {
-                  incumbent_solution->get(klass.i()) = solution->get(klass.i());
-                }
-                break;
-              }
-            }
-            return false;
-          } else {
-            return true;
-          }
-        });
-    std::cout << "number of leaf: " << leaf_count << std::endl;
-    return incumbent_solution;
+    search(0);
+    return solution_;
   }
 
 
@@ -138,18 +87,102 @@ private:
   }
 
 
+  Class::const_iterator maximum_feasible_item(const Class& klass)
+  {
+    auto result = klass.end();
+    PType obj(-1);
+    const int i = klass.i();
+
+    for (auto it = klass.begin(); it != klass.end(); ++it) {
+      const int j = *it;
+      const auto item = problem_.item(i, j);
+      if (is_feasible(c_stack_.begin() + d_ * (m_ - 1), item.w_begin(), d_)) {
+        if (item.p() > obj) {
+          obj = item.p();
+          result = it;
+        }
+      }
+    }
+
+    return result;
+  }
+
+
+  bool fathom(const int depth, const int i, const int j)
+  {
+    if (upper_bounds_->get(i, j) < optimal_value_) {
+      return false;
+    }
+
+    const auto item = problem_.item(i, j);
+    if (!is_feasible(c_stack_.begin() + d_ * depth, item.w_begin(), d_)) {
+      return false;
+    }
+
+    const PType ub = sub_lmck_problems_[depth]->solve(uc_stack_[depth] - mck_problem_->w(i, j));
+    if (obj_stack_[depth] + item.p() + ub < optimal_value_) {
+      return false;
+    }
+
+    return true;
+  }
+
+
+  void search(const int depth)
+  {
+    const Class& klass = index_.at(depth);
+    const int i = klass.i();
+    if (depth == m_ - 1) {
+      const auto maximum_j = maximum_feasible_item(klass);
+      if (maximum_j != klass.end()) {
+        const PType obj = obj_stack_.back() + problem_.p(i, *maximum_j);
+        if (obj > optimal_value_) {
+          optimal_value_ = obj;
+          std::cout << "new solution found : " << optimal_value_ + problem_.p_offset() << std::endl;
+          current_solution_->get(i) = *maximum_j;
+          for (const auto& klass : index_) {
+            solution_->get(klass.i()) = current_solution_->get(klass.i());
+          }
+        }
+      }
+    } else {
+      for (const int j : klass) {
+        const auto& item = problem_.item(i, j);
+        if (fathom(depth, i, j)) {
+          current_solution_->get(i) = j;
+          obj_stack_[depth + 1] = obj_stack_[depth] + item.p();
+          uc_stack_[depth + 1] = uc_stack_[depth] - mck_problem_->w(i, j);
+          for (const int k : constraint_index_) {
+            c_stack_[d_ * (depth + 1) + k]
+              = c_stack_[d_ * depth + k] - item.w(k);
+          }
+          search(depth + 1);
+        }
+      }
+    }
+  }
+
+
   const Problem<PType, WType>& problem_;
   Index index_;
   ConstraintIndex constraint_index_;
 
+  PType optimal_value_;
   ClassIndexedData<int>* solution_;
   ClassIndexedData<int>* current_solution_;
 
+  PType upper_bound_;
   IndexedData<PType>* upper_bounds_;
   mckp::Problem<PType, double>* mck_problem_;
   mckp::algorithm::LpRelaxationProblem<PType, double>* lmck_problem_;
   std::vector<mckp::algorithm::LpRelaxationProblem<PType, double>*> sub_lmck_problems_;
   ClassIndexedData<int>* base_solution_; // FIXME it must be delete
+
+  std::vector<PType> obj_stack_;
+  std::vector<WType> c_stack_;
+  std::vector<double> uc_stack_;
+  int d_;
+  int m_;
 };
 
 } // namespace
